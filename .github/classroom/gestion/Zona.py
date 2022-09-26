b862faff846e00ca3755ae69b759a5f2e39500ce
Yeferson05 <yaguilar@unal.edu.co>
2022-09-26 13:45:13 -0500

    .)

---
+++ .github/classroom/gestion/Zona.py
@@ -0,0 +1,29 @@
+class Zona:
+    def __init__(self, nombre, zoo):
+        self._nombre = nombre
+        self._zoo = zoo
+        self._animales = []
+
+    def agregarAnimales(self, animales): 
+        self._animales.append(animales)
+    
+    def cantidadAnimales(self): 
+        return len(self._animales)  
+
+    def getNombre(self): 
+        return self._nombre
+
+    def setNombre(self, nombre): 
+        self._nombre = nombre
+
+    def getUbicacion(self,): 
+        return self._ubicacion
+
+    def setUbicacion(self, ubicacion): 
+        self._ubicacion = ubicacion
+
+    def getZona(self): 
+        return self._zonas
+
+    def setZona(self, zonas): 
+        self._zonas = zonas
